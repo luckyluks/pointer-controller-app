3e5ac8944dfc616e3130219fd6d88886976fc4a3
luckyluks <lukas.rauh@r-a-u-h.de>
2020-10-06 22:29:15 +0200

    feat: adds face detection functionality

--- src/model_classes/facedetection_model.py
+++ src/model_classes/facedetection_model.py
@@ -1,32 +1,81 @@
+import sys
+import time
+import cv2
+
+import logging as log
+import numpy as np
+
 from src.model_classes.model import Model
+
 
 class FaceDetectionModel(Model):
     """
     Class for the Face Detection Model.
     """
-    # def __init__(self, model_name, device="CPU", extensions=None):
-    #     """
-    #     TODO: Use this to set your instance variables.
-    #     """
-    #     raise NotImplementedError
 
-    def predict(self, image):
+    def predict(self, image, request_id=0, draw_boxes=True):
         """
-        TODO: You will need to complete this method.
-        This method is meant for running predictions on the input image.
+        Predict the bounding box for the given image
         """
-        raise NotImplementedError
+        # Check valid input
+        if not isinstance(image, np.ndarray):
+            raise IOError("Image input is in the wrong format. Expected \"np.ndarray\"!")
+
+        # Pre-process the image
+        p_image = self.preprocess_input(image)
+
+        # Do the inference
+        predict_start_time = time.time()
+        self.network.start_async(request_id=request_id, 
+            inputs={self.input_blob: p_image}
+        )
+        inference_status = self.network.requests[request_id].wait(-1)
+        predict_end_time = (time.time() - predict_start_time) * 1000
+
+        # Process network output
+        if inference_status == 0:
+            outputs = self.network.requests[request_id].outputs[self.output_blob]
+            out_image, coords = self.preprocess_output(outputs, image, draw_box)
+            return out_image, coords, predict_end_time
 
     def preprocess_input(self, image):
         """
-        Before feeding the data into the model for inference,
-        you might have to preprocess it. This function is where you can do that.
+        Preprocess the image (use before feeding it to the network).
         """
-        raise NotImplementedError
+        # Get the input shape
+        _, _, height, width = self.input_shape
+        p_frame = cv2.resize(image, (width, height))
+        # Change data layout from HWC to CHW
+        p_frame = p_frame.transpose((2, 0, 1))
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+        return p_frame
 
-    def preprocess_output(self, outputs):
+    def preprocess_output(self, results, image, draw_boxes):
         """
-        Before feeding the output of this model to the next model,
-        you might have to preprocess the output. This function is where you can do that.
+        Process the output and draw detection results if applicable.
         """
-        raise NotImplementedError
+        # Check the model output blob shape
+        output_dims = results.shape
+        if (len(output_dims) != 4)  or (output_dims[3] != 7):
+            log.error("Incorrect output dimensions: {}".format(output_dims))
+            sys.exit(1)  
+
+        # Look up frame shapes
+        width = image.shape[1]
+        height = image.shape[0]
+
+        # Parse detection results
+        coords = []
+        for box in results[0][0]:  # 1x1xNx7
+            conf = box[2]
+            if conf >= self.probability_threshold:
+                xmin = int(box[3] * width)
+                ymin = int(box[4] * height)
+                xmax = int(box[5] * width)
+                ymax = int(box[6] * height)
+                coords.append((xmin, ymin, xmax, ymax))
+
+                # Draw detection box, if applicable
+                if draw_boxes:
+                    cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (0, 255, 0), 3)
+        return image, coords
